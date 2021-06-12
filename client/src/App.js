b9b367c82cba6d2eb15e1803a737915c7fd94331
jakkamd <jakkamdeepak5@gmail.com>
2021-06-12 09:05:03 -0600

    added api and other stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:7071/api/HttpTrigger")
+      .then(response => { return response.json() })
+      .then(data => setProducts(data));
+  }, [products])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {products.map(product => (<>
+        <p>{product.Name}</p>
+      </>))}
     </div>
   );
 }
