cdf7ee05acb22cde9f919a70ff493289067e0d12
jakkamd <jakkamdeepak5@gmail.com>
2021-06-13 13:34:48 -0600

    added comments and some minor changes.

--- api/HelperClass.cs
+++ api/HelperClass.cs
@@ -1,135 +1,129 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
-using System.Threading;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
-using System.Text;
 using Microsoft.Azure.Cosmos.Table;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.Extensions.Logging;
 
-namespace DCP.POC
-{
-    public class HelperClass
-    {
-        public static async Task<ImageWithText> QueryImage(CloudTable imageText_table, string imageName)
-        {
+namespace DCP.POC {
+    public class HelperClass {
 
-            TableQuery<ImageWithText> query = new TableQuery<ImageWithText>()
-               .Where(TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, imageName));
-            IEnumerable<ImageWithText> imageWithText = imageText_table.ExecuteQuery(query);
+        //Query the image by name(Rowkey) and return the Image blob url and text. 
+        public static async Task<ImageWithText> QueryImage (CloudTable imageText_table, string imageName) {
 
-            return imageWithText.ToList().FirstOrDefault();
+            TableQuery<ImageWithText> query = new TableQuery<ImageWithText> ()
+                .Where (TableQuery.GenerateFilterCondition ("RowKey", QueryComparisons.Equal, imageName));
+            IEnumerable<ImageWithText> imageWithText = imageText_table.ExecuteQuery (query);
+
+            return imageWithText.ToList ().FirstOrDefault ();
 
         }
 
-        public static async Task<TableResult> InsertTableEntity(CloudTable imageText_table, string imageUrl, string text, string fileName)
-        {
-            ImageWithText imageWithText = new ImageWithText(fileName);
+        //Inserts the image and text to Table storage.
+        public static async Task<TableResult> InsertTableEntity (CloudTable imageText_table, string imageUrl, string text, string fileName) {
+            ImageWithText imageWithText = new ImageWithText (fileName);
             imageWithText.Text = text;
             imageWithText.ImageUrl = imageUrl;
 
-            TableOperation insertOperation = TableOperation.InsertOrMerge(imageWithText);
-            TableResult result = await imageText_table.ExecuteAsync(insertOperation);
+            TableOperation insertOperation = TableOperation.InsertOrMerge (imageWithText);
+            TableResult result = await imageText_table.ExecuteAsync (insertOperation);
             return result;
         }
-        public static async Task<string> UploadFileBlobAsync(string connectionString, string blobContainerName, Stream content, string contentType, string fileName)
-        {
-            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
-            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
+
+        //Uploads the image to blob storage and return the url of the blob.
+        public static async Task<string> UploadFileBlobAsync (string connectionString, string blobContainerName, Stream content, string contentType, string fileName) {
+            BlobServiceClient blobServiceClient = new BlobServiceClient (connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient (blobContainerName);
             content.Position = 0;
-            var blobClient = containerClient.GetBlobClient(fileName);
-            await blobClient.UploadAsync(content, new BlobHttpHeaders { ContentType = contentType });
+            var blobClient = containerClient.GetBlobClient (fileName);
+            await blobClient.UploadAsync (content, new BlobHttpHeaders { ContentType = contentType });
             return blobClient.Uri.AbsoluteUri;
 
         }
 
-        public static async Task<Tuple<string, bool>> CheckIfBlobExists(string connectionString, string blobContainerName, string contentType, string fileName)
-        {
-            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
-            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
-            var blob = containerClient.GetBlobClient(fileName);
-            var doesBlobExist = await blob.ExistsAsync();
+        ///Checks if the image already exist in blob storage. it will return true or false
 
-            if (doesBlobExist)
-            {
-                return Tuple.Create(blob.Uri.AbsoluteUri, doesBlobExist.Value); ;
+        public static async Task<Tuple<string, bool>> CheckIfBlobExists (string connectionString, string blobContainerName, string contentType, string fileName) {
+            BlobServiceClient blobServiceClient = new BlobServiceClient (connectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient (blobContainerName);
+            var blob = containerClient.GetBlobClient (fileName);
+            var doesBlobExist = await blob.ExistsAsync ();
+
+            if (doesBlobExist) {
+                return Tuple.Create (blob.Uri.AbsoluteUri, doesBlobExist.Value);;
             }
-            return Tuple.Create("", doesBlobExist.Value); ;
+            return Tuple.Create ("", doesBlobExist.Value);;
 
         }
 
-        public static ComputerVisionClient Authenticate(string endpoint, string key)
-        {
+        public static ComputerVisionClient Authenticate (string endpoint, string key) {
             ComputerVisionClient client =
-              new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
-              { Endpoint = endpoint };
+                new ComputerVisionClient (new ApiKeyServiceClientCredentials (key)) { Endpoint = endpoint };
             return client;
         }
 
-        public static async Task<StringBuilder> ReadFileUrl(ComputerVisionClient client, string urlFile, ILogger log)
-        {
-            StringBuilder fullTextFromImage = new StringBuilder();
+        //Returns the text from the image thats stored in the blob storage.
+
+        public static async Task<StringBuilder> ReadFileUrl (ComputerVisionClient client, string urlFile, ILogger log) {
+            StringBuilder fullTextFromImage = new StringBuilder ();
             // Read text from URL
-            var textHeaders = await client.ReadAsync(urlFile);
+            var textHeaders = await client.ReadAsync (urlFile);
             // After the request, get the operation location (operation ID)
             string operationLocation = textHeaders.OperationLocation;
-            Thread.Sleep(2000);
+            Thread.Sleep (2000);
 
             const int numberOfCharsInOperationId = 36;
-            string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
+            string operationId = operationLocation.Substring (operationLocation.Length - numberOfCharsInOperationId);
 
             // Extract the text
             ReadOperationResult results;
-            log.LogInformation($"Extracting text from URL file {Path.GetFileName(urlFile)}...");
+            log.LogInformation ($"Extracting text from URL file {Path.GetFileName(urlFile)}...");
 
-            do
-            {
-                results = await client.GetReadResultAsync(Guid.Parse(operationId));
+            do {
+                results = await client.GetReadResultAsync (Guid.Parse (operationId));
             }
             while ((results.Status == OperationStatusCodes.Running ||
-                results.Status == OperationStatusCodes.NotStarted));
+                    results.Status == OperationStatusCodes.NotStarted));
 
             var textUrlFileResults = results.AnalyzeResult.ReadResults;
-            foreach (ReadResult page in textUrlFileResults)
-            {
-                foreach (Line line in page.Lines)
-                {
-                    log.LogInformation(line.Text);
-                    fullTextFromImage.Append(line.Text);
-                    fullTextFromImage.Append(Environment.NewLine);
+            foreach (ReadResult page in textUrlFileResults) {
+                foreach (Line line in page.Lines) {
+                    log.LogInformation (line.Text);
+                    fullTextFromImage.Append (line.Text);
+                    fullTextFromImage.Append (Environment.NewLine);
                 }
             }
             return fullTextFromImage;
         }
 
-        public static async Task<List<ImageWithText>> GetAllImages(CloudTable imageText_table)
-        {
-            TableQuery<ImageWithText> query = new TableQuery<ImageWithText>()
-            .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "ImageWithText"));
-            IEnumerable<ImageWithText> imageWithText = imageText_table.ExecuteQuery(query);
+        //GEt all images with text that are stored in Table Storage
+        public static async Task<List<ImageWithText>> GetAllImages (CloudTable imageText_table) {
+            TableQuery<ImageWithText> query = new TableQuery<ImageWithText> ()
+                .Where (TableQuery.GenerateFilterCondition ("PartitionKey", QueryComparisons.Equal, "ImageWithText"));
+            IEnumerable<ImageWithText> imageWithText = imageText_table.ExecuteQuery (query);
 
-            return imageWithText.ToList();
+            return imageWithText.ToList ();
         }
 
-        public static async Task<Dictionary<string, Uri>> GetImagesFromBlobContainer(string connectionString, string containerName)
-        {
+        public static async Task<Dictionary<string, Uri>> GetImagesFromBlobContainer (string connectionString, string containerName) {
 
-            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+            BlobServiceClient blobServiceClient = new BlobServiceClient (connectionString);
 
-            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient (containerName);
 
-            Dictionary<string, Uri> names = new Dictionary<string, Uri>();
+            Dictionary<string, Uri> names = new Dictionary<string, Uri> ();
 
-            await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
-            {
-                // Console.WriteLine("\t" + blobItem.Name);
-                BlobClient blobClient = containerClient.GetBlobClient(blobItem.Name);
-                names.Add(blobItem.Name, blobClient.Uri);
+            await
+            foreach (BlobItem blobItem in containerClient.GetBlobsAsync ()) {
+                BlobClient blobClient = containerClient.GetBlobClient (blobItem.Name);
+                names.Add (blobItem.Name, blobClient.Uri);
             }
             return names;
         }
--- api/UploadImage.cs
+++ api/UploadImage.cs
@@ -1,100 +1,86 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
+using api.Models;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using Microsoft.Azure.Cosmos.Table;
-using api.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
-namespace DCP.POC
-{
-    public static class UploadImage
-    {
-        [FunctionName("UploadImage")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("UploadImage function processed a request.");
-            try
-            {
-                var imageData = await req.ReadFormAsync();
+namespace DCP.POC {
+    public static class UploadImage {
+
+        //Validate and upload the image 
+        [FunctionName ("UploadImage")]
+        public static async Task<IActionResult> Run (
+            [HttpTrigger (AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log) {
+            log.LogInformation ("UploadImage function processed a request.");
+            try {
+                var imageData = await req.ReadFormAsync ();
                 var image = req.Form.Files["file"];
                 byte[] imageDataToUpload;
                 string blobUrl;
                 ImageToTextResponse response;
-                if (image.ContentType.Contains("image"))
-                {
+                if (image.ContentType.Contains ("image")) {
 
-                    string storageConnectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
-                    string containerName = Environment.GetEnvironmentVariable("AZURE_CONTAINER_NAME");
+                    string storageConnectionString = Environment.GetEnvironmentVariable ("AZURE_STORAGE_CONNECTION_STRING");
+                    string containerName = Environment.GetEnvironmentVariable ("AZURE_CONTAINER_NAME");
 
-                    string computerVisionConnectionString = Environment.GetEnvironmentVariable("AZURE_COMPUTER_VISION_CONNECTION_STRING");
-                    string computerVisionEndpoint = Environment.GetEnvironmentVariable("AZURE_COMPUTER_VISION_ENDPOINT");
+                    string computerVisionConnectionString = Environment.GetEnvironmentVariable ("AZURE_COMPUTER_VISION_CONNECTION_STRING");
+                    string computerVisionEndpoint = Environment.GetEnvironmentVariable ("AZURE_COMPUTER_VISION_ENDPOINT");
 
-                    string tableName = Environment.GetEnvironmentVariable("AZURE_TABLE_NAME");
+                    string tableName = Environment.GetEnvironmentVariable ("AZURE_TABLE_NAME");
 
-                    using (var target = new MemoryStream())
-                    {
-                        image.CopyTo(target);
-                        imageDataToUpload = target.ToArray();
+                    using (var target = new MemoryStream ()) {
+                        image.CopyTo (target);
+                        imageDataToUpload = target.ToArray ();
 
-                        var doesBlobAlreadyExist = await HelperClass.CheckIfBlobExists(storageConnectionString, containerName, image.ContentType, image.FileName);
-                        CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
-                        CloudTableClient tableClient = storageAccount.CreateCloudTableClient(new TableClientConfiguration());
-                        CloudTable imageToTextTable = tableClient.GetTableReference(tableName);
+                        var doesBlobAlreadyExist = await HelperClass.CheckIfBlobExists (storageConnectionString, containerName, image.ContentType, image.FileName);
+                        CloudStorageAccount storageAccount = CloudStorageAccount.Parse (storageConnectionString);
+                        CloudTableClient tableClient = storageAccount.CreateCloudTableClient (new TableClientConfiguration ());
+                        CloudTable imageToTextTable = tableClient.GetTableReference (tableName);
 
-                        if (!doesBlobAlreadyExist.Item2)
-                        {
-                            blobUrl = await HelperClass.UploadFileBlobAsync(storageConnectionString, containerName, target, image.ContentType, image.FileName);
+                        if (!doesBlobAlreadyExist.Item2) {
+                            blobUrl = await HelperClass.UploadFileBlobAsync (storageConnectionString, containerName, target, image.ContentType, image.FileName);
 
-                            ComputerVisionClient client = HelperClass.Authenticate(computerVisionEndpoint, computerVisionConnectionString);
+                            ComputerVisionClient client = HelperClass.Authenticate (computerVisionEndpoint, computerVisionConnectionString);
 
-                            var text = await HelperClass.ReadFileUrl(client, blobUrl, log);
+                            var text = await HelperClass.ReadFileUrl (client, blobUrl, log);
 
-                            var tableInsertResponse = await HelperClass.InsertTableEntity(imageToTextTable, blobUrl, text.ToString(), image.FileName);
+                            var tableInsertResponse = await HelperClass.InsertTableEntity (imageToTextTable, blobUrl, text.ToString (), image.FileName);
 
-                            response = new ImageToTextResponse()
-                            {
+                            response = new ImageToTextResponse () {
                                 Url = blobUrl,
-                                Text = text.ToString()
+                                Text = text.ToString ()
                             };
-                            return new OkObjectResult(JsonConvert.SerializeObject(response));
-                        }
-                        else
-                        {
-                            var imageUrlWithText = await HelperClass.QueryImage(imageToTextTable, image.FileName);
-                            if (imageUrlWithText != null)
-                            {
-                                response = new ImageToTextResponse()
-                                {
-                                    Url = imageUrlWithText.ImageUrl,
-                                    Text = imageUrlWithText.Text
+                            return new OkObjectResult (JsonConvert.SerializeObject (response));
+                        } else {
+                            var imageUrlWithText = await HelperClass.QueryImage (imageToTextTable, image.FileName);
+                            if (imageUrlWithText != null) {
+                                response = new ImageToTextResponse () {
+                                Url = imageUrlWithText.ImageUrl,
+                                Text = imageUrlWithText.Text
                                 };
-                                return new OkObjectResult(JsonConvert.SerializeObject(response));
+                                return new OkObjectResult (JsonConvert.SerializeObject (response));
                             }
 
                         }
 
-                        return new BadRequestResult();
+                        return new BadRequestResult ();
 
                     }
+                } else {
+                    return new UnsupportedMediaTypeResult ();
                 }
-                else
-                {
-                    return new UnsupportedMediaTypeResult();
-                }
-            }
-            catch (System.Exception ex)
-            {
-                log.LogInformation("Error Message", ex.Message);
+            } catch (System.Exception ex) {
+                log.LogInformation ("Error Message", ex.Message);
 
-                return new BadRequestResult();
+                return new BadRequestResult ();
 
             }
 
