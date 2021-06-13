16827d0daf0607b374f07b9575f86bb0ff82958e
jakkamd <jakkamdeepak5@gmail.com>
2021-06-13 15:48:45 -0600

    .

--- client/src/AllImages.js
+++ client/src/AllImages.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import ImageTextCard from "./ImageTextCard";
 import Grid from "@material-ui/core/Grid";
 import { Container } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 export default function AllImages() {
   const [imagesWithText, setImagesWithText] = useState([]);
@@ -20,20 +21,33 @@ export default function AllImages() {
       {
         <Container>
           <Grid container spacing={6}>
-            {imagesWithText.map((imagesWithText) => (
-              <Grid
-                key={imagesWithText.ImageUrl}
-                xs={12}
-                sm={6}
-                item
-                style={{ minHeight: "100px" }}
-              >
-                <ImageTextCard
-                  imageUrl={imagesWithText.ImageUrl}
-                  imageText={imagesWithText.Text}
-                />
-              </Grid>
-            ))}
+            {imagesWithText.length > 0 ? (
+              imagesWithText.map((imagesWithText) => (
+                <Grid
+                  key={imagesWithText.ImageUrl}
+                  xs={12}
+                  sm={6}
+                  item
+                  style={{ minHeight: "100px" }}
+                >
+                  <ImageTextCard
+                    imageUrl={imagesWithText.ImageUrl}
+                    imageText={imagesWithText.Text}
+                  />
+                </Grid>
+              ))
+            ) : (
+              <CircularProgress
+                color="primary"
+                style={{
+                  width: "140px",
+                  height: "140px",
+                  marginLeft: "200px",
+                  marginTop: "200px",
+                }}
+                thickness={2}
+              />
+            )}
           </Grid>
         </Container>
       }
